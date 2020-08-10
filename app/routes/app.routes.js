d28dfe7fe30df379bfdb3c52a1750582554bf749
prashantk <pr463475@dal.ca>
2020-08-09 21:02:45 -0300

    final version

--- app/routes/app.routes.js
+++ app/routes/app.routes.js
@@ -11,21 +11,5 @@ module.exports = (app) => {
   router.get("/", watches.findAll);
   // Create a new record
   router.post("/newOrder", order.create);
-
-  // // Retrieve all published watches
-  // router.get("/published", watches.findAllPublished);
-
-  // // Retrieve a single record with id
-  // router.get("/:id", watches.findOne);
-
-  // // Update a record with id
-  // router.put("/:id", watches.update);
-
-  // // Delete a record with id
-  // router.delete("/:id", watches.delete);
-
-  // // Delete all watches
-  // router.delete("/", watches.deleteAll);
-
   app.use("/api/watches", router);
 };
